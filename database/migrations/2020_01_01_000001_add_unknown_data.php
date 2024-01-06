3b633efc24e634b1347ee7da34581c20726f869e
Tim Haak <tim@haak.co>
2024-01-06 17:44:00 +0200

    Fix migration name

--- database/migrations/2020_01_01_000001_add_unknown_data.php
+++ database/migrations/2020_01_01_000001_add_unknown_data.php
@@ -1,15 +1,15 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
-class HaakcoLaravelLocationManagerAddUnkownLocations extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         DB::unprepared(
             file_get_contents(
@@ -23,7 +23,7 @@ class HaakcoLaravelLocationManagerAddUnkownLocations extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         DB::unprepared(
             file_get_contents(
@@ -31,4 +31,4 @@ class HaakcoLaravelLocationManagerAddUnkownLocations extends Migration
             )
         );
     }
-}
+};
